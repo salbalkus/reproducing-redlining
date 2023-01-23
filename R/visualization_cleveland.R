33e17991981d4e57fa8af777d4d2d04bffe76a43
Salvador Balkus <45985925+salbalkus@users.noreply.github.com>
2023-01-22 20:59:06 -0500

    Update visualization_cleveland.R

--- R/visualization_cleveland.R
+++ R/visualization_cleveland.R
@@ -1,14 +1,19 @@
+# This file produces the Cleveland bar plot visualization
+
 library(ggplot2)
 library(dplyr)
 library(tidyr)
 
 cleveland_bar <- function(metro_grades){
+  # Filter to Cleveland
   cleveland <- metro_grades %>% filter(metro_area == "Cleveland-Elyria, OH") %>% select(holc_grade, pct_white, pct_black, pct_hisp, pct_asian, pct_other) %>% pivot_longer(pct_white:pct_other)
+  # Convert data to correct factors for plotting
   cleveland$holc_grade <- factor(cleveland$holc_grade, levels=rev(c("A","B","C","D")))
   cleveland$holc_grade <- recode(cleveland$holc_grade, A="\"Best\"", B="\"Desirable\"", C="\"Declining\"", D="\"Hazardous\"")
   cleveland$name <- recode(cleveland$name, pct_white="White", pct_black="Black", pct_hisp="Latino", pct_asian="Asian", pct_other = "Other")
   cleveland$name <- factor(cleveland$name, levels=rev(c("White", "Black", "Latino", "Asian", "Other")))
   
+  # Plot the data
   ggplot(cleveland) + geom_bar(aes(x = value, y = holc_grade, fill = name), color="white", lwd=1, width=0.8, stat="identity", show.legend=T) +
     theme_void() +
     theme(axis.text.y = element_text(),
